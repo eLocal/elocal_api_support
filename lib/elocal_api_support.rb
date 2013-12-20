f852206d8839b272540073a9b8926924a62e811b
Rob Di Marco <rob.dimarco@416software.com>
2013-12-20 13:14:16 -0500

    Add requires

--- lib/elocal_api_support.rb
+++ lib/elocal_api_support.rb
@@ -1,5 +1,6 @@
-require "elocal_api_support/version"
-
-module ElocalApiSupport
-  # Your code goes here...
-end
+require 'elocal_api_support/version'
+require 'active_support/concern.rb'
+require 'elocal_api_support/authorization'
+require 'elocal_api_support/enable_cors'
+require 'elocal_api_support/model_from_params'
+module ElocalApiSupport; end
