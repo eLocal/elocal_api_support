6eb780ff198e5619b5501c2ffe5384ffdaf94723
Rob Di Marco <rob.dimarco@416software.com>
2013-12-20 13:54:11 -0500

    Create an all import

--- lib/elocal_api_support.rb
+++ lib/elocal_api_support.rb
@@ -3,4 +3,17 @@ require 'active_support/concern.rb'
 require 'elocal_api_support/authorization'
 require 'elocal_api_support/enable_cors'
 require 'elocal_api_support/model_from_params'
-module ElocalApiSupport; end
+require 'elocal_api_support/actions'
+
+module ElocalApiSupport
+  module All
+    extend ActiveSupport::Concern
+
+    included do
+      include ElocalApiSupport::Actions
+      include ElocalApiSupport::Authorization
+      include ElocalApiSupport::EnableCors
+      include ElocalApiSupport::ModelFromParams
+    end
+  end
+end
