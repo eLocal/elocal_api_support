774c14ca99a330be990dc4489dc15fdb174a4a1c
Rob Di Marco <rob.dimarco@416software.com>
2014-01-03 16:49:23 -0500

    Update per_page/total_pages to pull from filtered object

--- lib/elocal_api_support/actions/index.rb
+++ lib/elocal_api_support/actions/index.rb
@@ -4,9 +4,9 @@ module ElocalApiSupport
       def index
         render json: {
             current_page:  current_page,
-            per_page:      per_page,
+            per_page:      filtered_objects.respond_to?(:per_page) ? filtered_objects.per_page : per_page,
             total_entries: filtered_objects.total_count,
-            total_pages:   (params[:page].present? || params[:per_page].present?) ? filtered_objects.total_pages : 1,
+            total_pages:   filtered_objects.respond_to?(:total_pages) ? filtered_objects.total_pages : 1,
             records:       filtered_objects_for_json
           }
       end
