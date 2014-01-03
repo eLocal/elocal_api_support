58fb237834d6b2f817bdcaabda9908b358702ab1
Rob Di Marco <rob.dimarco@416software.com>
2014-01-03 17:13:51 -0500

    Use correct value

--- lib/elocal_api_support/actions/index.rb
+++ lib/elocal_api_support/actions/index.rb
@@ -4,7 +4,7 @@ module ElocalApiSupport
       def index
         render json: {
             current_page:  current_page,
-            per_page:      (params[:page].present? || params[:per_page].present?) ? filtered_objects.per_page : filtered_objects.total_count,
+            per_page:      (params[:page].present? || params[:per_page].present?) ? per_page : filtered_objects.total_count,
             total_entries: filtered_objects.total_count,
             total_pages:   (params[:page].present? || params[:per_page].present?) ? filtered_objects.total_pages : 1,
             records:       filtered_objects_for_json
