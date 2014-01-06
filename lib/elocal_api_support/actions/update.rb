1d433325da15fac8be1fee0c8a0d69ff89d53ce4
Rob Di Marco <rob.dimarco@416software.com>
2014-01-06 13:17:06 -0500

    Return 422 errors if error on updating

--- lib/elocal_api_support/actions/update.rb
+++ lib/elocal_api_support/actions/update.rb
@@ -6,7 +6,7 @@ module ElocalApiSupport
         if lookup_object.update_attributes(params[associated_model_name])
           render json: lookup_object
         else
-          render json:  {errors: lookup_object.errors}
+          render json:  {errors: lookup_object.errors}, status: 422
         end
       end
     end
