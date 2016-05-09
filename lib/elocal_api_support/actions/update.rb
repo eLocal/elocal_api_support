47d34eee829c4e152766774b6c6cfaad84ee503e
Rob Di Marco <rob@elocal.com>
2016-05-09 14:39:54 -0400

    Ignore certain columns from being updated

--- lib/elocal_api_support/actions/update.rb
+++ lib/elocal_api_support/actions/update.rb
@@ -3,11 +3,21 @@ module ElocalApiSupport
     module Update
       def update
         params.permit!
-        if lookup_object.update_attributes(params[associated_model_name])
+        if lookup_object.update_attributes(parameters_available_for_update)
           render json: lookup_object
         else
           render json:  { errors: lookup_object.errors }, status: 422
         end
+      end
+
+      private
+
+      def parameters_available_for_update
+        params[associated_model_name].reject { |(k, _v)| k.in?(parameters_to_ignore_from_update) }
+      end
+
+      def parameters_to_ignore_from_update
+        %w(id created_at updated_at).freeze
       end
     end
   end
