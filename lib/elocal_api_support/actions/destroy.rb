f25b77c8f331ff32b697403b6b09bcb8fed8dd2f
Rob Di Marco <rob@elocal.com>
2016-11-21 09:29:04 -0500

    [finished #134689987] Do not destroy if nil

--- lib/elocal_api_support/actions/destroy.rb
+++ lib/elocal_api_support/actions/destroy.rb
@@ -2,13 +2,18 @@ module ElocalApiSupport
   module Actions
     module Destroy
       def destroy
+        # record not found is ok for destroy
+        render json: { errors: "Cannot retrieve record: #{params[:id]}" } and return \
+          if lookup_object.nil?
+
         if lookup_object.destroy
           render json: lookup_object
         else
           render json: { errors: "Failed to destroy #{lookup_object}" }, status: 500
         end
       rescue ActiveRecord::RecordNotFound
-        render json: {} # record not found is ok for destroy
+        # record not found is ok for destroy
+        render json: { errors: "Cannot retrieve record: #{params[:id]}" }
       end
     end
   end
